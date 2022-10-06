9f8460a479a07835d89b9c5e70a6c451afd5a0dd
jonas-andrade <jonas6116m@outlook.com>
2022-10-06 14:32:21 -0300

    componente de imagens

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Image } from 'react-native';
-
-import img from "./assets/asta.jpg";
-import Component_ from './Component.js'
+import { StyleSheet, View, } from 'react-native';
+import Component_ from './Component.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Aula de React Native </Text>
-
-      <Component_ /> 
-
-      <Image
-      style={styles.img}
-      source={img}
-      ></Image>
+   
+   <Component_></Component_>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -24,13 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3f3b4c',
+    
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  img: {
-    width: 1000,
-    height: 150
-  },
-  titulo: { color: '#0B5E70', fontSize: 24, marginBottom: 24}, 
+  }
 });
--- Component.js
+++ Component.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { StyleSheet, Text, Pressable, Image, View} from 'react-native';
+
 
 export default function Component_() {
+    // não consegui resolver esse problema de contagem sem o useState()
+    let [num ,setNum] = useState(0)
+    const [foto ,setFoto] = useState(require(`./assets/Thumbnails/${num}.jpg`))
+    function changeFoto(){
+        if(num>=44){setNum(num=0);} setNum(++num);
+        setFoto(require(`./assets/Thumbnails/${num}.jpg`));
+        document.querySelector(".css-accessibilityImage-9pa8cd").src = foto
 
+        //DEBUG:
+        console.log("useState: "+foto)
+        console.log(document.querySelector("img"))
+       
+       }
     return (
-        <View style={styles.comp}>
-            <Text style={styles.subt}>aula de componente em react native</Text>
-        </View>
+     <View style={styles.comp}>
+
+    <Text style={styles.til}>MEUS FILMES E SÉRIES</Text>
+    <Pressable onPress={changeFoto}>
+    <Image style={styles.img}  source={foto}/>
+    <Text style={styles.subt}>@Jonas Medeiros Andrade</Text>
+    </Pressable>
+     </View>
+
     );
 }
 
-
 const styles = StyleSheet.create({
     comp: {
-        backgroundColor: '#0B5E70',
-        padding: 12,
-        borderRadius: 3,
-        marginBottom: 30
+
+        backgroundColor: '#5d4f5e',
+        padding: 10,
+        borderRadius: 30,
+        maxHeight: '100%'
+    },
+    til: {
+        color: '#2f2b3a',
+        fontSize: 25,
+        fontWeight: 'bold',
     },
     subt: {
-        color: '#ddd',
+        color: '#959ba9',
         fontSize: 18,
-        fontWeight: 'bold',
-    }
+        fontWeight: '100',
+        marginTop: 5
+    },
+    img: {
+     width: 340,
+     height: 540,
+     borderRadius: 20,
+     marginTop: 20,
+     
+
+     
+    },
+      
 });
